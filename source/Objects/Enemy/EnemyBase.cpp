8f61dc79b81876137b8df150175b43caf30c3b24
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-04-18 13:57:55 +0900

    [update] EnemyBaseClassの関数と変数宣言

--- source/Objects/Enemy/EnemyBase.cpp
+++ source/Objects/Enemy/EnemyBase.cpp
@@ -1,1 +1,29 @@
 #include "EnemyBase.h"
+
+EnemyBase::EnemyBase()
+{
+}
+
+EnemyBase::~EnemyBase()
+{
+}
+
+void EnemyBase::Initialize()
+{
+}
+
+void EnemyBase::Update(float delta_second)
+{
+}
+
+void EnemyBase::Draw(const Vector2D& screen_offset) const
+{
+}
+
+void EnemyBase::Finalize()
+{
+}
+
+void EnemyBase::OnHitCollision(GameObjectBase* hit_object)
+{
+}
