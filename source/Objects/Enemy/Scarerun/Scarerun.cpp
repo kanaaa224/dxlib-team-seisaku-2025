a9897df44ce21db26b1c74490041c92f4087500e
issei-toyomoto <120348418+issei-toyomoto@users.noreply.github.com>
2025-04-20 19:30:06 +0900

    [update] スケアランの関数作成

--- source/Objects/Enemy/Scarerun/Scarerun.cpp
+++ source/Objects/Enemy/Scarerun/Scarerun.cpp
@@ -1,1 +1,29 @@
 #include "Scarerun.h"
+
+Scarerun::Scarerun()
+{
+}
+
+Scarerun::~Scarerun()
+{
+}
+
+void Scarerun::Initialize()
+{
+}
+
+void Scarerun::Update(float delta_second)
+{
+}
+
+void Scarerun::Draw(const Vector2D& screen_offset) const
+{
+}
+
+void Scarerun::Finalize()
+{
+}
+
+void Scarerun::OnHitCollision(GameObjectBase* hit_object)
+{
+}
--- source/Objects/Enemy/Scarerun/Scarerun.h
+++ source/Objects/Enemy/Scarerun/Scarerun.h
@@ -1,5 +1,18 @@
 #pragma once
-class Scarerun
+
+#include "../EnemyBase.h"
+
+class Scarerun : public EnemyBase
 {
+public:
+	Scarerun();
+	~Scarerun();
+
+	void Initialize() override;
+	void Update(float delta_second) override;
+	void Draw(const Vector2D& screen_offset) const override;
+	void Finalize() override;
+
+	void OnHitCollision(GameObjectBase* hit_object) override;
 };
 
